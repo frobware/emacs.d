72ed533411efac3f0f1815612d11a2f0c573f8de
Andrew McDermott <aim@frobware.com>
2014-10-04 07:00:08 +0100

    Ensure exec-path-from-shell is installed on first use

--- init.el
+++ init.el
@@ -69,11 +69,13 @@
   :ensure t)
 
 (use-package exec-path-from-shell
-  :config
+  :ensure t
+  :init
   (progn
     (dolist (var '("GOPATH" "SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE"))
-      (add-to-list 'exec-path-from-shell-variables var))
-    (exec-path-from-shell-initialize)))
+      (add-to-list 'exec-path-from-shell-variables var)))
+  :config
+  (exec-path-from-shell-initialize))
 
 (use-package ag
   :ensure t
