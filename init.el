dd87b61303cfe1d15b809ec183d2f9a884b577db
Andrew McDermott <aim@frobware.com>
2015-07-12 20:15:40 +0100

    Checkpoint

--- init.el
+++ init.el
@@ -526,3 +526,16 @@ This doesn't support the chanserv auth method"
     (ansi-color-apply-on-region (point-min) (point-max))))
 
 (server-start)
+
+(defvar dark-background nil)
+
+(defun toggle-dark-background ()
+  (interactive)
+  (let ((difficult-colors
+         '("red" "blue" "medium blue")))
+    (mapc
+     (lambda (face)
+       (and (member (face-attribute face :foreground)  difficult-colors)
+            (set-face-bold-p face (not dark-background))))
+     (face-list)))
+  (setq dark-background (not dark-background)))
