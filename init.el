afc9dab692020c5ade6cf233b8ff0a511f408558
Andrew McDermott <aim@frobware.com>
2016-11-10 21:28:29 +0000

    Remove commented out cruft

--- init.el
+++ init.el
@@ -484,19 +484,6 @@
 (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
 (add-to-list 'interpreter-mode-alist '("python" . python-mode))
 
-;; (setq interpreter-mode-alist
-;;       (cons '("python" . python-mode) interpreter-mode-alist)
-;;       python-mode-hook '(lambda () (progn
-;;				     (set-variable 'py-indent-offset 4)
-;;				     (set-variable 'indent-tabs-mode t))))
-
-(setq company-tooltip-limit 20)                      ; bigger popup window
-(setq company-idle-delay .3)                         ; decrease delay before autocompletion popup shows
-(setq company-echo-delay 0)                          ; remove annoying blinking
-(setq company-begin-commands '(self-insert-command)) ; start autocompletion only after typing
-
-(setq twittering-icon-mode t)
-
 (require 'ansi-color)
 (add-to-list 'auto-mode-alist '("\\.log\\'" . display-ansi-colors))
 (add-to-list 'auto-mode-alist '("\\.log\\'" . log4j-mode))
