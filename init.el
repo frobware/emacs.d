01515511a7a938eef299af71133ab09c8a8d3471
Andrew McDermott <aim@frobware.com>
2017-02-16 08:06:56 +0000

    Whitespace cleanup

--- init.el
+++ init.el
@@ -523,12 +523,12 @@
 ;;   (when (getenv "DISPLAY")
 ;;     (defun xclip-cut-function (text &optional push)
 ;;       (with-temp-buffer
-;; 	(insert text)
-;; 	(call-process-region (point-min) (point-max) "xclip" nil 0 nil "-i" "-selection" "clipboard")))
+;;	(insert text)
+;;	(call-process-region (point-min) (point-max) "xclip" nil 0 nil "-i" "-selection" "clipboard")))
 ;;     (defun xclip-paste-function()
 ;;       (let ((xclip-output (shell-command-to-string "xclip -o -selection clipboard")))
-;; 	(unless (string= (car kill-ring) xclip-output)
-;; 	  xclip-output )))
+;;	(unless (string= (car kill-ring) xclip-output)
+;;	  xclip-output )))
 ;;     (setq interprogram-cut-function 'xclip-cut-function)
 ;;     (setq interprogram-paste-function 'xclip-paste-function))
 ;;   (defun terminal-init-screen ()
@@ -541,8 +541,8 @@
 ;;   (defun setup-color-theme-p ()
 ;;     "Returns true if it looks like the display can handle 24-bit colors"
 ;;     (or (display-graphic-p)
-;; 	(< 256 (display-color-cells))
-;; 	(getenv "KONSOLE_DBUS_SESSION")))
+;;	(< 256 (display-color-cells))
+;;	(getenv "KONSOLE_DBUS_SESSION")))
 ;;   (defun setup-color-theme ()
 ;;     "Set up my color theme"
 ;;     (when (setup-color-theme-p)
@@ -555,7 +555,7 @@
 
 (use-package server
   :ensure t
-  :config 
+  :config
   (unless (server-running-p)
     (server-start)))
 
