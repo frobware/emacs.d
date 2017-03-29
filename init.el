f859011b5d18a2cb6d99bb7255a652cc7a108dc1
Andrew McDermott <aim@frobware.com>
2017-03-29 15:32:48 +0100

    set c-default-style

--- init.el
+++ init.el
@@ -602,3 +602,20 @@
   :ensure t)
 
 (message "Done")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(c-default-style
+   (quote
+    ((c-mode . "linux")
+     (java-mode . "java")
+     (awk-mode . "awk")
+     (other . "gnu")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
