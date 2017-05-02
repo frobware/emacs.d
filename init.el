b3cdc3fcc1e027adb1fca8c4a0998b29264e5828
Andrew McDermott <aim@frobware.com>
2017-05-02 12:56:56 +0100

    Remove old helm-gtags configuration

--- init.el
+++ init.el
@@ -669,22 +669,6 @@
 
 (setq company-tooltip-align-annotations t)
 
-;; (and nil
-;;      (use-package helm-gtags
-;;        :ensure t
-;;        :config
-;;        (progn
-;; 	 (define-key helm-gtags-mode-map (kbd "M-t") 'helm-gtags-find-tag)
-;; 	 (define-key helm-gtags-mode-map (kbd "M-r") 'helm-gtags-find-rtag)
-;; 	 (define-key helm-gtags-mode-map (kbd "M-s") 'helm-gtags-find-symbol)
-;; 	 (define-key helm-gtags-mode-map (kbd "M-g M-p") 'helm-gtags-parse-file)
-;; 	 (define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
-;; 	 (define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
-;; 	 (define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
-;; 	 (add-hook 'c-mode-hook 'helm-gtags-mode)
-;; 	 (add-hook 'c++-mode-hook 'helm-gtags-mode)
-;; 	 (add-hook 'asm-mode-hook 'helm-gtags-mode))))
-
 (use-package rtags
   :ensure t
   :config
@@ -757,21 +741,12 @@
           helm-gtags-pulse-at-cursor t
           helm-gtags-prefix-key "\C-cg"
           helm-gtags-suggested-key-mapping t)
-
-    ;; Enable helm-gtags-mode in Dired so you can jump to any tag
-    ;; when navigate project tree with Dired
     (add-hook 'dired-mode-hook 'helm-gtags-mode)
-
-    ;; Enable helm-gtags-mode in Eshell for the same reason as above
     (add-hook 'eshell-mode-hook 'helm-gtags-mode)
-
-    ;; Enable helm-gtags-mode in languages that GNU Global supports
     (add-hook 'c-mode-hook 'helm-gtags-mode)
     (add-hook 'c++-mode-hook 'helm-gtags-mode)
     (add-hook 'java-mode-hook 'helm-gtags-mode)
     (add-hook 'asm-mode-hook 'helm-gtags-mode)
-
-    ;; key bindings
     (with-eval-after-load 'helm-gtags
       (define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
       (define-key helm-gtags-mode-map (kbd "C-j") 'helm-gtags-select)
