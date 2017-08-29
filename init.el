1b7dc218a8e94edebe2d9c3faa0156a8702a0af7
Andrew McDermott <aim@frobware.com>
2017-08-29 07:26:46 +0100

    Custom hi-line-mode

--- init.el
+++ init.el
@@ -32,8 +32,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(highlight ((t (:background "gray30"))))
- '(hl-line ((t (:inherit highlight)))))
+ '(hl-line ((t (:background "grey30")))))
 
 ;; Perls of wisdom:
 ;;   http://emacshorrors.com/posts/come-in-and-find-out.html
