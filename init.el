5767ea89c023a4368fb3aac19a9a6c2464d68bf1
Andrew McDermott <aim@frobware.com>
2018-02-14 11:20:36 +0000

    ediff colouring

--- init.el
+++ init.el
@@ -76,6 +76,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ediff-even-diff-A ((t (:background "dim gray"))))
+ '(ediff-even-diff-B ((t (:background "dim gray"))))
+ '(ediff-odd-diff-B ((t (:background "dim gray"))))
  '(hl-line ((t (:background "grey30")))))
 
 ;; Perls of wisdom:
