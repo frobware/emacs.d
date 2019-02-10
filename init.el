0316d8ec940c19e98858968d8ec267c4c4a318e2
Andrew McDermott <aim@frobware.com>
2019-02-10 23:44:33 +0000

    Set frame-background-mode to nil (auto detect)

--- init.el
+++ init.el
@@ -65,6 +65,7 @@
  '(custom-safe-themes
    (quote
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(frame-background-mode nil)
  '(global-magit-file-mode nil)
  '(helm-gtags-prefix-key "g")
  '(helm-gtags-suggested-key-mapping t)
