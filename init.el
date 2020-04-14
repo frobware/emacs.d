3f08c5670c36cab8ef0fe7d8d0c7aab0474fc183
Andrew McDermott <aim@frobware.com>
2020-04-14 21:47:16 +0100

    use-package ediff
    Helps cleanup some compiler warnings

--- init.el
+++ init.el
@@ -638,13 +638,15 @@ save it in `ffap-file-at-point-line-number' variable."
       (goto-line ffap-file-at-point-line-number))
     (setq ffap-file-at-point-line-number nil)))
 
-;; Pressing d will copy both A and B to buffer C.
-(defun ediff-copy-both-to-C ()
-  (interactive)
-  (ediff-copy-diff ediff-current-difference nil 'C nil
-		   (concat
-		    (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
-		    (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
+(use-package ediff
+  :config
+  ;; Pressing d will copy both A and B to buffer C.
+  (defun ediff-copy-both-to-C ()
+    (interactive)
+    (ediff-copy-diff ediff-current-difference nil 'C nil
+		     (concat
+		      (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
+		      (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer)))))
 
 (defun add-d-to-ediff-mode-map ()
   (define-key ediff-mode-map "d" 'ediff-copy-both-to-C))
