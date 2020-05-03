ea0942adbdab4bd9fbe2ea5bb5be919510c66149
Andrew McDermott <aim@frobware.com>
2020-05-03 09:55:35 +0100

    setup

---
+++ setup.bash
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Copied from straight/radon
+
+set -e
+
+script="$(realpath "$0")"
+srcdir="$(dirname "$script")"
+
+safe_link() {
+    if [[ -e "$2" && ! -L "$2" ]]; then
+        echo "already exists and not a symlink: $2" >&2
+        exit 1
+    fi
+
+    ln -sf "$1" "$2"
+}
+
+mkdir -p "$HOME/.emacs.d/straight/versions"
+safe_link "$srcdir/emacs/early-init.el" "$HOME/.emacs.d/early-init.el"
+safe_link "$srcdir/emacs/init.el" "$HOME/.emacs.d/init.el"
+safe_link "$srcdir/emacs/versions.el" \
+          "$HOME/.emacs.d/straight/versions/frobware.el"
