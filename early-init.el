b563059d4874905e47d302496512de77369b8f58
Andrew McDermott <aim@frobware.com>
2020-06-02 08:56:34 +0100

    set package archives to nil for Emacs 28

--- early-init.el
+++ early-init.el
@@ -3,6 +3,8 @@
 ;; Emacs HEAD (27+) introduces early-init.el, which is run before init.el,
 ;; before package and UI initialization happens.
 
+(setq package-archives nil)
+
 ;; Defer garbage collection further back in the startup process
 (setq gc-cons-threshold most-positive-fixnum)
 
