7bcc11fd91da1abf1ef3d9a11dbcc26bca46883f
Andrew McDermott <aim@frobware.com>
2020-12-07 21:40:02 +0000

    use-package exec-path-from-shell

--- init.el
+++ init.el
@@ -72,6 +72,12 @@
   :config
   (gcmh-mode 1))
 
+(use-package exec-path-from-shell
+  :defer nil
+  :config
+  (exec-path-from-shell-initialize))
+
+
 (use-package select
   :straight (:type built-in)
   :defer nil
