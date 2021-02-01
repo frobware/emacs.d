093c441d3237b970287d76766239fba9df92e3b5
Andrew McDermott <aim@frobware.com>
2021-02-01 16:03:08 +0000

    Add totally-unacceptable

--- terminal-keys.sh
+++ terminal-keys.sh
@@ -21,3 +21,13 @@ defaults write -app Terminal NSUserKeyEquivalents -dict-add 'Cut' '^$X'
 defaults write -app Terminal NSUserKeyEquivalents -dict-add 'Paste' '^$V'
 defaults write -app Terminal NSUserKeyEquivalents -dict-add 'Close Window' '\0'
 defaults write -app Terminal NSUserKeyEquivalents -dict-add 'Close Tab' '\0'
+
+function totally-unacceptable {
+    # Catalina's notarization is a disaster for cli apps (aka the real world).
+    sudo spctl --master-disable
+
+    # and the show goes on.
+    DevToolsSecurity -enable
+}
+
+totally-unacceptable
