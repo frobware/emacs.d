6f09afe121fe3dd10e88b055d4d084dcbcf75b2f
Andrew McDermott <aim@frobware.com>
2021-02-14 17:51:16 +0000

    bump terminal keys to be more Linux like

--- terminal-keys.sh
+++ terminal-keys.sh
@@ -16,13 +16,26 @@ defaults write -g NSUserKeyEquivalents -dict-add 'Emoji & Symbols' '\0'
 # 	 "Paste"="^$V";
 # }'
 
-defaults write -app Terminal NSUserKeyEquivalents -dict-add 'Copy' '^$C'
 defaults write -app Terminal NSUserKeyEquivalents -dict-add 'Cut' '^$X'
+defaults write -app Terminal NSUserKeyEquivalents -dict-add 'Copy' '^$C'
 defaults write -app Terminal NSUserKeyEquivalents -dict-add 'Paste' '^$V'
-defaults write -app Terminal NSUserKeyEquivalents -dict-add 'Close Window' '\0'
-defaults write -app Terminal NSUserKeyEquivalents -dict-add 'Close Tab' '\0'
 
-function totally-unacceptable {
+defaults write -app Terminal NSUserKeyEquivalents -dict-add 'Close Tab' '\0'
+defaults write -app Terminal NSUserKeyEquivalents -dict-add 'Close Window' '\0'
+
+defaults write -app Safari NSUserKeyEquivalents -dict-add 'Cut' '^X'
+defaults write -app Safari NSUserKeyEquivalents -dict-add 'Copy' '^C'
+defaults write -app Safari NSUserKeyEquivalents -dict-add 'Paste' '^V'
+defaults write -app Safari NSUserKeyEquivalents -dict-add 'Close Tab' '^w'
+
+#$ defaults delete Safari NSUserKeyEquivalents
+
+defaults write -g NSUserKeyEquivalents -dict-add 'Cut' '^X'
+defaults write -g NSUserKeyEquivalents -dict-add 'Copy' '^C'
+defaults write -g NSUserKeyEquivalents -dict-add 'Paste' '^V'
+defaults write -g NSUserKeyEquivalents -dict-add 'Close Tab' '^w'
+
+function totally_unacceptable {
     # Catalina's notarization is a disaster for cli apps (aka the real world).
     sudo spctl --master-disable
 
@@ -30,4 +43,4 @@ function totally-unacceptable {
     DevToolsSecurity -enable
 }
 
-totally-unacceptable
+totally_unacceptable
