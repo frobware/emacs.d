73423c018becd9da6aca32ea812529b22e1ad6e7
Andrew McDermott <aim@frobware.com>
2021-05-12 12:07:26 +0100

    terminal-keys: remove XQuartz shortcuts

--- terminal-keys.sh
+++ terminal-keys.sh
@@ -13,6 +13,15 @@ defaults write -g NSUserKeyEquivalents -dict-add 'Emoji & Symbols' '\0'
 
 defaults write -app Emacs NSUserKeyEquivalents -dict-add 'Emoji & Symbols' '\0'
 
+defaults write -app XQuartz NSUserKeyEquivalents -dict-add 'Copy' '\0'
+defaults write -app XQuartz NSUserKeyEquivalents -dict-add 'Close' '\0'
+defaults write -app XQuartz NSUserKeyEquivalents -dict-add 'Minimize' '\0'
+defaults write -app XQuartz NSUserKeyEquivalents -dict-add 'Terminal' '\0'
+defaults write -app XQuartz NSUserKeyEquivalents -dict-add 'Preferences' '\0'
+defaults write -app XQuartz NSUserKeyEquivalents -dict-add 'Hide X11' '\0'
+defaults write -app XQuartz NSUserKeyEquivalents -dict-add 'Hide Others' '\0'
+defaults write -app XQuartz NSUserKeyEquivalents -dict-add 'Quit X11' '\0'
+
 # defaults write -app Terminal NSUserKeyEquivalents '{
 # 	 "Copy"="^$C";
 # 	 "Paste"="^$V";
