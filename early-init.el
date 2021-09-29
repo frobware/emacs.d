51e9336c33e8f763c634c7c0521a59248ba19103
Andrew McDermott <aim@frobware.com>
2021-09-29 09:57:02 +0100

    early-init: treat spicy and x1c the same

--- early-init.el
+++ early-init.el
@@ -23,7 +23,8 @@
   (add-to-list 'default-frame-alist '(internal-border-width . 5))
   (add-to-list 'default-frame-alist '(fullscreen . maximized)))
 
-(and (string-equal system-name "spicy")
+(and (or (string-equal system-name "spicy")
+	 (string-equal system-name "x1c"))
      (progn
        (add-to-list 'default-frame-alist '(font . "JetBrains Mono-14"))
        (add-to-list 'default-frame-alist '(undecorated . t))
