8f2bd5ae19f9feb9e2d6061ccb6e6538121a4d19
Andrew McDermott <aim@frobware.com>
2022-03-13 21:10:51 +0000

    Foribly use straight packages

--- init.el
+++ init.el
@@ -160,7 +160,7 @@
 (defvar use-nix-epkgs (or (string= system-name "mba")
 			  (string= system-name "x1c")))
 
-(setq use-nix-epkgs (string= system-name "mba"))
+(setq use-nix-epkgs nil)
 
 (when (not use-nix-epkgs)
   (setq-default straight-vc-git-default-clone-depth 1)
