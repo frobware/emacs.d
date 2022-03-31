b47fb74abc56fdf02d8a21fa3d8e5f7faf1c8572
Andrew McDermott <aim@frobware.com>
2022-03-31 22:16:45 +0100

    Set modus-themes-region: accented
    Not sure if this is actually deprecated.

--- init.el
+++ init.el
@@ -442,7 +442,7 @@
         ;; Options for `modus-themes-region' are either nil (the default),
         ;; or a list of properties that may include any of those symbols:
         ;; `no-extend', `bg-only', `accented'
-        modus-themes-region '(bg-only no-extend)
+        modus-themes-region '(accented)
 
         ;; Options for `modus-themes-diffs': nil, 'desaturated, 'bg-only
         modus-themes-diffs 'desaturated
