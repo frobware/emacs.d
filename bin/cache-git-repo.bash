5d9b69f82506a82796e09237dc39edde279b6f6c
Andrew McDermott <aim@frobware.com>
2022-08-27 21:31:19 +0100

    bin/cache-git-repo: use "real" git for cloning

--- bin/cache-git-repo.bash
+++ bin/cache-git-repo.bash
@@ -2,6 +2,12 @@
 
 set -eu
 
+: "${REAL_GIT:=/etc/profiles/per-user/aim/bin/git}"
+
+if [[ -x /usr/bin/git ]]; then
+    REAL_GIT=/usr/bin/git
+fi
+
 : "${CACHE_DIR:=$HOME/.cache/git/repositories}"
 
 for url in "$@"; do
@@ -15,6 +21,6 @@ for url in "$@"; do
     if [[ ! -d "$path" ]]; then
         project_dir="$(dirname "$path")"
         echo mkdir -p "${project_dir}"
-        echo /usr/bin/git -C "${project_dir}" clone --bare "${url} ${url##*/}.git"
+        echo ${REAL_GIT} -C "${project_dir}" clone --bare "${url} ${url##*/}.git"
     fi
 done
